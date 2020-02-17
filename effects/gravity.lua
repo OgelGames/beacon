ad28a6d1e73687dca4c6bf0480bf7b9cc49a275b
OgelGames <olliverdc28@gmail.com>
2020-02-17 21:16:55 +1100

    add gravity effects

---
+++ effects/gravity.lua
@@ -0,0 +1,56 @@
+
+-- Gravity - 25%, 50% or 200% of the current gravity
+--------------------------------------------------
+
+local function on_apply(player, multiplier, id)
+	if beacon.has_player_monoids then
+		player_monoids.gravity:add_change(player, multiplier, id)
+	else
+		local physics = player:get_physics_override()
+		physics.gravity = physics.gravity * multiplier
+		player:set_physics_override(physics)
+	end
+end
+
+local function on_remove(player, multiplier, id)
+	if beacon.has_player_monoids then
+		player_monoids.gravity:del_change(player, id)
+	else
+		local physics = player:get_physics_override()
+		physics.gravity = physics.gravity / multiplier
+		player:set_physics_override(physics)
+	end
+end
+
+beacon.register_effect("gravityquarter", {
+	desc_name = "Quarter Gravity",
+	min_level = 1,
+	on_apply = function(player)
+		on_apply(player, 0.25, "beacon_gravityquarter")
+	end,
+	on_remove = function(player)
+		on_remove(player, 0.25, "beacon_gravityquarter")
+	end,
+})
+
+beacon.register_effect("gravityhalf", {
+	desc_name = "Half Gravity",
+	min_level = 1,
+	on_apply = function(player)
+		on_apply(player, 0.5, "beacon_gravityhalf")
+	end,
+	on_remove = function(player)
+		on_remove(player, 0.5, "beacon_gravityhalf")
+	end,
+})
+
+beacon.register_effect("gravitydouble", {
+	desc_name = "Double Gravity",
+	min_level = 1,
+	on_apply = function(player)
+		on_apply(player, 2, "beacon_gravitydouble")
+	end,
+	on_remove = function(player)
+		on_remove(player, 2, "beacon_gravitydouble")
+	end,
+})
