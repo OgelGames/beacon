cf5e37bde14a60b8e5ab820f159c6ee7a73f5165
OgelGames <olliverdc28@gmail.com>
2020-02-18 22:32:46 +1100

    use sorted effect ids for `/beacon_effects` command

--- chatcommands.lua
+++ chatcommands.lua
@@ -1,8 +1,18 @@
 
+local function get_all_defs()
+	local defs = {}
+	for _,id in ipairs(beacon.sorted_effect_ids) do
+		table.insert(defs, beacon.effects[id])
+	end
+	return defs
+end
+
 local function get_player_defs(name)
 	local defs = {}
-	for id,_ in pairs(beacon.player_effects[name].active) do
-		table.insert(defs, beacon.effects[id])
+	for _,id in ipairs(beacon.sorted_effect_ids) do
+		if beacon.player_effects[name].active[id] then
+			table.insert(defs, beacon.effects[id])
+		end
 	end
 	return defs
 end
@@ -13,7 +23,7 @@ minetest.register_chatcommand("beacon_effects", {
 	func = function(name, param)
 		if param == "all" then
 			local output = {"All avalible beacon effects:"}
-			for _,def in pairs(beacon.effects) do
+			for _,def in ipairs(get_all_defs()) do
 				table.insert(output, "- "..def.desc_name)
 			end
 			return true, table.concat(output, "\n")
@@ -25,7 +35,7 @@ minetest.register_chatcommand("beacon_effects", {
 			return true, table.concat(output, "\n")
 		elseif param == "all_info" then
 			local output = {"All avalible beacon effects:"}
-			for _,def in pairs(beacon.effects) do
+			for _,def in ipairs(get_all_defs()) do
 				table.insert(output, "- "..def.desc_name.." -- "..def.info)
 			end
 			return true, table.concat(output, "\n")
